85480929eb5ff8b4916237b6edb348a393100271
Nikolaj Sievertsen Nøring <94489550+NikoMash@users.noreply.github.com>
2024-03-24 13:04:24 +0100

    Loadbalancer

---
+++ LoadBalancer/Controllers/LoadBalancerController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadBalancer.Controllers;
+
+[ApiController]
+[Route("api/search/")]
+public class LoadBalancerController : ControllerBase
+{
+    private static readonly string[] servers =  {
+        "http://localhost:7055/api/search",
+        "http://localhost:7056/api/search"
+    };
+
+    private static int next = 0;
+
+    [HttpGet]
+    [Route("{query}/{maxAmount}/{caseSensitive}")]
+    public string Get(string query, int maxAmount, bool caseSensitive)
+        {
+
+            string server = $"{servers[next]}/{query}/{maxAmount}/{caseSensitive}";
+            next = (next + 1) % servers.Length;
+
+            Response.Redirect(server);
+            return server;
+        }
+}
